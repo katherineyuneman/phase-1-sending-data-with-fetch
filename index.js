539f538bad4853e0370c8c67ac432690ce666c34
KatherineYuneman <kyuneman@gmail.com>
2021-11-10 15:57:28 -0500

    commit tree 2b5ade36e636ec10ef3c8306e0d84b6d8df08899

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+
+
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", postNewUser)
+  .then(res => res.json())
+  .then(function (newUser){
+    let p = document.createElement('p')
+    p.textContent = newUser.id
+    document.body.appendChild.p
+  })
+}
+
+
+const postNewUser = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com"
+  })
+};
+
+submitData()
+
+
+
+
+
+
+// DOG EXAMPLE
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
+
+
